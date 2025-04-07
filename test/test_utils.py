a8f785707daaf5cf07a776064d462e148f0434f9
Didier Durand <durand.didier@gmail.com>
2025-04-07 07:53:50 +0200

    commit message

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,7 +6,14 @@ from utils import exec_os_command
 
 class TestUtils(unittest.TestCase):
 
-    def test_exec_os_command(self):
+    def test_exec_os_command_ko(self):
+            exception, rc, stdout, stderr = exec_os_command("foo-bar-foo")
+            self.assertTrue(isinstance(exception, FileNotFoundError))
+            self.assertIsNone(rc)
+            self.assertIsNone(stderr)
+            self.assertIsNone(stdout)
+
+    def test_exec_os_command_ok(self):
         if platform in ["linux","linux2","darwin"]:
             exception, rc, stdout, stderr = exec_os_command("uname -a")
             self.assertIsNone(exception)
@@ -16,4 +23,4 @@ class TestUtils(unittest.TestCase):
             if platform == "darwin":
                 self.assertTrue("Darwin" in stdout)
             if platform in  ["linux","linux2"]:
-                self.assertTrue("Darwin" in stdout)
+                self.assertTrue("Darwin" in stdout,f"uname output: {stdout}")
