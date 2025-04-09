53620cb2fdeb5eef6ebfd0761abef26700de6d0f
Didier Durand <durand.didier@gmail.com>
2025-04-09 08:37:16 +0200

    commit message

--- test/test_server.py
+++ test/test_server.py
@@ -1,3 +1,6 @@
+import pytest
+import pytest_asyncio
+from mcp import Tool
 from mcp.server import FastMCP
 
 from mcp_testcase import McpTestCase  # pylint: disable=E0401
@@ -9,10 +12,19 @@ class TestServer(McpTestCase):
     def test_startup_checks(self):
         self.assertTrue(startup_checks())
 
+    @pytest_asyncio.fixture
+    async def get_mcp_server(self):
+        return mcp_server
+
+    @pytest.mark.asyncio
     async def test_server_instance(self):
+        server = await self.get_mcp_server()
         self.assertIsNotNone(mcp_server)
-        self.assertTrue(isinstance(mcp_server, FastMCP))
+        self.assertTrue(isinstance(server, FastMCP))
         #
-        tools = await mcp_server.list_tools()
+        tools = await server.list_tools()
         self.assertTrue(isinstance(tools, list))
         self.assertEqual(len(tools), 1)
+        tool = tools[0]
+        self.assertIsInstance(tool, Tool)
+        self.assertEqual("", tool.name)
